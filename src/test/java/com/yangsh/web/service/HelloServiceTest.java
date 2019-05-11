c57fb461a06baecf222ae632e01b0889ac1abbcf
yangsh <samyangsh@163.com>
2019-05-11 23:22:12 +0800

    xxx

---
+++ src/test/java/com/yangsh/web/service/HelloServiceTest.java
@@ -0,0 +1,21 @@
+package com.yangsh.web.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Description: TODO
+ * <p></p>
+ * Author: shaoye
+ * Date: 2019-05-11 23:19
+ */
+public class HelloServiceTest {
+
+    @Test
+    public void hello() {
+        HelloService service = new HelloService();
+
+        Assert.assertEquals(service.hello(), "Hello");
+    }
+
+}
